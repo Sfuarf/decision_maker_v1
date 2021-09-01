a5b6d04395e40f6b197bffbdf8c26037736759b9
Sfuarf <s.s.carrick@gmail.com>
2021-09-01 22:36:32 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/decision_maker_v1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.decision_maker_v1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:decision_maker_v1/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meditation App',
+      theme: ThemeData(
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+      ),
+      home: MyHomePage(
+        title: 'Decision Maker!',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              // image: DecorationImage(
+              //   alignment: Alignment.centerLeft,
+              //   image: AssetImage("assets/images/cafe_icon.png"),
+              // ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Choose Your Location!",
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  SearchBar(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
